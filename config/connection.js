11a8c950dbf45cdf9e678a0c39b739faa4353649
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-02-24 02:20:08 -0500

    changed connection to jaws

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,27 @@
 const mysql = require("mysql");
 const password = require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  // NOTE: Be sure to add your MySQL password here!
-  password: process.env.PASSword,
-  database: "burger_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    // NOTE: Be sure to add your MySQL password here!
+    password: process.env.PASSword,
+    database: "burger_db",
+  });
+}
 
-// Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+connection.connect();
+// connection.connect((err) => {
+//   if (err) {
+//     console.error(`error connecting: ${err.stack}`);
+//     return;
+//   }
+//   console.log(`connected as id ${connection.threadId}`);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
