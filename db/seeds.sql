27144c9b2fcdcecbc9c6e8567fcf2f56f16e600d
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-02-23 23:17:58 -0500

    added a quotation marK

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_types) VALUES ('Bacon-Bacon Burger');
 INSERT INTO burgers (burger_types) VALUES ('French Dip Burger');
-INSERT INTO burgers (burger_types) VALUES ('Veggie Burger);
+INSERT INTO burgers (burger_types) VALUES ('Veggie Burger');
